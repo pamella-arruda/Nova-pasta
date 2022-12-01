4f1c534bd343999c0cc37a1cc1d1a06d4e1eea04
Pamella Arruda <pamella.frota01@gmail.com>
2022-11-30 22:40:23 -0300

    Iniciando JS

--- index.js
+++ index.js
@@ -1,12 +1,36 @@
-var nome
-var peso
-var altura
-var imc
+const calcular = document.getElementById('calcular');
 
-function calcular() {
-    nome = document.querySelector('#nome').value;
-    peso = document.querySelector('#peso').value;
-    altura = document.querySelector('#altura').value;
-    imc = (peso/(altura*altura)).toFixed(2);
+function imc () {
+    const nome = document.getElementById('nome');
+    const altura = document.getElementById('altura');
+    const peso = document.getElementById('peso');
+    const resultado = document.getElementById('resultado');
+
+if (nome !== '' && altura !== '' && peso !== ''){
+     
+    const valorIMC = (peso / (altura * altura)).toFixed(2);
+
+    let cassificacao = "";
+
+    if(valorIMC < 18.5){ 
+        classificacao = 'a baixo do peso.';
+    }else if(valorIMC < 24.9){
+        classificacao = 'com peso ideal. Parabéns!';
+    }else if(valorIMC < 29.9){
+        classificacao = 'com sobrepeso.';
+    }else if(valorIMC < 34.9){
+        classificacao = 'com obesidade moderada.';
+    }else if(valorIMC < 39.9){
+        classificacao = 'com obesidade severa.';
+    }else if(valorIMC > 40){
+        classificacao = 'com obesidade morbida. Cuidado!';
+    }
+
+    resultado.textContente = `${nome} seu IMC é ${valorIMC} e você está ${cassificação}` 
+
+}else {
+    resultado.textContente = 'Preencha todos os campos!';
 }
-
+}
+    
+calcular.addEventListener('clik', imc);
